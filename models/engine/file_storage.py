31d9ce8b361d8fbcac62a3fd51e1c818accc5b29
Andrescortes23 <andresccortes23@icloud.com>
2020-11-01 04:20:23 +0000

    Corrections

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,6 @@
 import json
 import os.path
-
+#from models.base_model import BaseModel
 
 class FileStorage:
 
@@ -15,13 +15,23 @@ class FileStorage:
         self.__objects[key] = obj
 
     def save(self):
-        newdic =
+        newdict = {}
+        for key, value in self.__objects.items():
+            newdict[key] = value.to_dict()
+
         with open(self.__file_path, "w") as thefile:
-            json.dump(self.__objects, thefile)
+            json.dump(newdict, thefile)
 
     def reload(self):
         if os.path.isfile(self.__file_path):
+            from models.base_model import BaseModel
+
             with open(self.__file_path, "r") as thefile:
-                self.__objects = json.load(thefile)
+                objectfromjson = json.load(thefile)
+
+            for key, value in objectfromjson.items():
+                cls = value["__class__"]
+                obj = eval(cls + "(**value)")
+                self.__objects[key] = obj
         else:
             pass
