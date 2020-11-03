025892929d7436d3420d35ecae8b89f713d7ee54
Andrescortes23 <andresccortes23@icloud.com>
2020-11-03 21:21:34 +0000

    Act

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -42,8 +42,12 @@ class FileStorage:
                 objectfromjson = json.load(thefile)
 
             for key, value in objectfromjson.items():
-                if value["__class__"] == "User":
-                    from models.user import User
+                from models.user import User
+                from models.place import Place
+                from models.state import State
+                from models.city import City
+                from models.amenity import Amenity
+                from models.review import Review
                 cls = value["__class__"]
                 obj = eval(cls + "(**value)")
                 self.__objects[key] = obj
