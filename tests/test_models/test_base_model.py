1afa6456050648ebfb728768eca7bee594185d09
Andrescortes23 <andresccortes23@icloud.com>
2020-11-04 19:19:06 +0000

    Test

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Module to test the class BaseModel
+"""
+import unittest
+from models.base_model import BaseModel
+
+
+class Test_BaseModel(unittest.TestCase):
+    """
+    Tests for BaseModel
+    """
+
+    def
