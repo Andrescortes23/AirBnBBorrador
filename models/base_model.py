f7ad9cb3b1891703a85870b61ab296d222dbd96e
Andrescortes23 <andresccortes23@icloud.com>
2020-11-04 06:17:57 +0000

    Corrections

--- models/base_model.py
+++ models/base_model.py
@@ -25,7 +25,8 @@ class BaseModel:
             for key, value in kwargs.items():
                 if (key == "created_at" or key == "updated_at"):
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                self.__dict__[key] = value
+                if key != "__class__":
+                    setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
@@ -33,19 +34,25 @@ class BaseModel:
             storage.new(self)
 
     def __str__(self):
-        """Method to return a string representation"""
+        """
+        Method to return a string representation
+        """
 
         return "[{}] ({}) {}".format(self.__class__.__name__,
                                      self.id, self.__dict__)
 
     def save(self):
-        """Method to update the attribute update_at"""
+        """
+        Method to update the attribute update_at
+        """
 
         self.updated_at = datetime.now()
         storage.save()
 
     def to_dict(self):
-        """Method to return a dictionary representation of our object"""
+        """
+        Method to return a dictionary representation of our object
+        """
 
         dicto = self.__dict__.copy()
         dicto["__class__"] = self.__class__.__name__
